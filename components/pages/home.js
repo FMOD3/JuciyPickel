17f4a24b0cd22c0bda46c39a90715539733a65de
Jacob <70965524+jOaawd@users.noreply.github.com>
2025-02-19 12:10:45 -0800

    Update home.js

--- components/pages/home.js
+++ components/pages/home.js
@@ -1,8 +1,5 @@
 export default function HomePage() {
     return (
-        <>
-            <h1>Example Title</h1>
-            <p>This is a short example paragraph.</p>
-        </>
-    );
+        <p>Home</p>
+    )
 }
